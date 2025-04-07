5ebc1609719f3d2c8efc68fa38193d99be24d55c
Dhruv Malviya <dhruvnotfoundlol@gmail.com>
2025-04-07 10:51:26 +0530

    mysql

---
+++ server.js
@@ -0,0 +1,31 @@
+// server.js
+const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// MySQL connection
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
